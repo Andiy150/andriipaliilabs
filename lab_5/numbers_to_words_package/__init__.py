8c96575031b28a9879403bd997863170ae581c3e
Andrii Palii <andrii.palii.tr.2022@lpnu.ua>
2023-05-15 21:26:15 +0300

    Lab5 add code

---
+++ lab_5/numbers_to_words_package/__init__.py
@@ -0,0 +1,9 @@
+"""
+The numbers_to_words_package.
+
+This package contains modules for converting numbers to words
+and handling user input.
+"""
+
+from .numbers_to_words import convert_num_to_word
+from .input_handler import get_user_input
